beed84416de24ba69d68a88c3968e29d0cb7bd0e
altierispeixoto <altieris.marcelino@gmail.com>
2019-10-26 18:32:36 -0300

    configure spark Dockerfile + neo4j dataimport

--- airflow/simple-app/load_from_postgresql.py
+++ airflow/simple-app/load_from_postgresql.py
@@ -1,10 +1,3 @@
-import os
-
-#directories = os.listdir("/home/altieris/master-thesis/airflow/data/raw/")
-
-directories = os.listdir("/data/raw/")
-
-
 from argparse import ArgumentParser
 from sparketl import ETLSpark
 etlspark = ETLSpark()
@@ -16,9 +9,8 @@ parser.add_argument("-f", "--file", dest="table",
 args = parser.parse_args()
 print(args.table)
 
-source_path = "/data/raw/{}".format(args.table)
 target_path = "/data/processed/{}".format(args.table)
 
-raw_data = etlspark.extract(source_path)
-etlspark.load_to_database(raw_data, args.table)
-etlspark.transform(raw_data, target_path)
+tbl = '{}_stg'.format(args.table)
+df = etlspark.load_from_database(tbl)
+etlspark.save(df, target_path, coalesce=1, format="csv")
--- airflow/simple-app/sparketl.py
+++ airflow/simple-app/sparketl.py
@@ -2,8 +2,9 @@
 #
 # findspark.init()
 
-#spark-submit --driver-class-path . SimpleApp.py
+# spark-submit --driver-class-path . load_to_postgresql.py
 
+from pyspark.sql import DataFrameReader
 from pyspark.conf import SparkConf
 from pyspark.context import SparkContext
 from pyspark.sql import SQLContext
@@ -34,7 +35,9 @@ class ETLSpark:
 
         split = functions.split(df['filename'], '_')
 
-        df =  df.withColumn('datareferencia', functions.concat(split.getItem(0), functions.lit("-"), split.getItem(1), functions.lit("-") , split.getItem(2)))
+        df = df.withColumn('datareferencia', functions.to_date(
+            functions.concat(split.getItem(0), functions.lit("-"), split.getItem(1), functions.lit("-"),
+                             split.getItem(2)), 'yyyy-MM-dd'))
 
         dropcolumns = ["filepath", "filename"]
         df = df.toDF(*[c.lower() for c in df.columns]).drop(*dropcolumns)
@@ -42,7 +45,6 @@ class ETLSpark:
 
     @staticmethod
     def load_to_database(df, tablename):
-
         print(df.show(5))
 
         db_properties = {}
@@ -51,13 +53,24 @@ class ETLSpark:
         db_properties['password'] = "airflow"
         db_properties['driver'] = "org.postgresql.Driver"
 
-        tabletarget = 'public.{}'.format(tablename)
-
+        tabletarget = 'public.{}_stg'.format(tablename)
         # Save the dataframe to the table.
         df.write.jdbc(url=db_url, table=tabletarget, mode='overwrite', properties=db_properties)
 
+    def load_from_database(self, tablename):
+        db_properties = {}
+        db_url = "jdbc:postgresql://10.5.0.3:5432/dw?user=airflow&password=airflow"
+        db_properties['username'] = "airflow"
+        db_properties['password'] = "airflow"
+        db_properties['driver'] = "org.postgresql.Driver"
 
-    def transform(self, src_data, target_path, coalesce=1):
-        src_data.coalesce(coalesce).write.mode('overwrite').format("parquet").save(target_path)
+        df =  self.sqlContext.read.jdbc(url=db_url,table= "(select * from linhas_stg limit 10) as q1", properties=db_properties)
+
+        #df = DataFrameReader(self.sqlContext).jdbc(url=db_url, table=tablesource, properties=db_properties)
+        print(df.show(5))
+        return df
+
+    def save(self, src_data, target_path, coalesce=1, format="parquet"):
+        src_data.coalesce(coalesce).write.mode('overwrite').format(format).save(target_path)
         del src_data
         gc.collect()
