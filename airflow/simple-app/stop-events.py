ccea80e3f6e411ea81bc04a41489afcac8c9152e
altierispeixoto <altieris.marcelino@gmail.com>
2019-11-19 07:27:00 -0300

    update tvg - relationships [vehicle/schedule, vehicle/stop]

--- airflow/simple-app/stop-events.py
+++ airflow/simple-app/stop-events.py
@@ -7,7 +7,7 @@ events_processed = etlspark.sqlContext.read.parquet('/data/processed/eventsproce
 
 evt = events_processed.select('cod_linha', col('veic').alias('vehicle'), col('event_timestamp').alias('stop_timestamp'),'year','month','day', 'hour','minute','second',
                         date_format('event_timestamp', 'hh:mm:ss').alias('event_time'), col('lat').alias('latitude'), col('lon').alias('longitude')) \
-    .filter(col('moving_status') == 'STOPPED')
+    .filter(col('moving_status') == 'STOPPED').orderBy('vehicle','event_timestamp')
 
 target_path = "/data/processed/{}".format("stopevents")
 etlspark.save(evt, target_path, coalesce=1, format="csv")
