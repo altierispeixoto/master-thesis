c5f24f2def56d23b616cdf035e612c97cdaf02d6
altierispeixoto <altieris.marcelino@gmail.com>
2020-06-12 13:25:13 -0300

    refactoring data architecture - download files dag

--- airflow/dags/download_files.py
+++ airflow/dags/download_files.py
@@ -12,18 +12,16 @@ import yaml
 import os
 import shutil
 
-
 config = yaml.load(open('./dags/config/data.yml'), Loader=yaml.FullLoader)
 
 
 def download_files(ds, folder, file, **kwargs):
-    pprint("Date range: {}".format(date_range))
-    pprint("Base URL: {}".format(base_url))
-    pprint("Filename: {}".format(file))
+    pprint(f"Date range: {date_range}")
+    pprint(f"Base URL: {base_url}")
+    pprint(f"Filename: {file}")
 
-    fmt = "%Y-%m-%d"
-    sdate = datetime.strptime(date_range['date_start'], fmt)
-    edate = datetime.strptime(date_range['date_end'], fmt)
+    sdate = datetime.strptime(date_range['date_start'], "%Y-%m-%d")
+    edate = datetime.strptime(date_range['date_end'], "%Y-%m-%d")
 
     delta = edate - sdate
 
@@ -31,16 +29,16 @@ def download_files(ds, folder, file, **kwargs):
         day = sdate + timedelta(days=i)
         download_file_day = day.strftime("%Y_%m_%d")
 
-        datareferencia = day.replace(day=1).strftime("%Y-%m-%d")
+        datareferencia = day.replace(day=1).strftime("%Y-%m")
 
-        url = '{}{}_{}'.format(base_url, download_file_day, file)
-        pprint("Downloading: {}".format(url))
+        url = f"{base_url}{download_file_day}_{file}"
+        pprint(f"Downloading: {url}")
 
-        base_folder = 'data/staging/{datareferencia}/{folder}'.format(folder=folder, datareferencia=datareferencia)
+        base_folder = f"data/staging/{datareferencia}/{folder}"
 
         os.makedirs(base_folder, exist_ok=True)
 
-        fd = 'data/staging/{datareferencia}/{folder}/{download_file_day}_{file}'.format(datareferencia = datareferencia,folder=folder, download_file_day = download_file_day, file = file)
+        fd = f"data/staging/{datareferencia}/{folder}/{download_file_day}_{file}"
 
         http = urllib3.PoolManager()
         r = http.request('GET', url, preload_content=False)
@@ -53,18 +51,17 @@ def download_files(ds, folder, file, **kwargs):
                 out.write(data)
 
         r.release_conn()
+
     return "download realizado com sucesso"
 
 
 def decompress_files(ds, folder, file, **kwargs):
+    pprint(f"Date range: {date_range}")
+    pprint(f"Base URL: {base_url}")
+    pprint(f"Filename: {file}")
 
-    pprint("Date range: {}".format(date_range))
-    pprint("Base URL: {}".format(base_url))
-    pprint("Filename: {}".format(file))
-
-    fmt = "%Y-%m-%d"
-    sdate = datetime.strptime(date_range['date_start'], fmt)
-    edate = datetime.strptime(date_range['date_end'], fmt)
+    sdate = datetime.strptime(date_range['date_start'], "%Y-%m-%d")
+    edate = datetime.strptime(date_range['date_end'], "%Y-%m-%d")
 
     delta = edate - sdate
 
@@ -72,26 +69,27 @@ def decompress_files(ds, folder, file, **kwargs):
         day = sdate + timedelta(days=i)
         download_file_day = day.strftime("%Y_%m_%d")
 
-        datareferencia = day.replace(day=1).strftime("%Y-%m-%d")
+        datareferencia = day.replace(day=1).strftime("%Y-%m")
 
-        base_folder = 'data/raw/{datareferencia}/{folder}'.format(datareferencia = datareferencia, folder = folder)
+        base_folder = f"data/raw/{datareferencia}/{folder}"
 
         os.makedirs(base_folder, exist_ok=True)
 
         try:
-            fstaging = 'data/staging/{datareferencia}/{folder}/{download_file_day}_{file}'.format(datareferencia=datareferencia, folder=folder, download_file_day=download_file_day,file=file)
-            fraw = '{}/{}_{}'.format(base_folder, download_file_day, file.replace('.xz', ''))
+            fstaging = f"data/staging/{datareferencia}/{folder}/{download_file_day}_{file}"
+            fraw = f"{base_folder}/{download_file_day}_{file.replace('.xz', '')}"
 
             binary_data_buffer = lzma.open(fstaging, mode='rt', encoding='utf-8').read()
 
             with open(fraw, 'w') as a:
                 a.write(binary_data_buffer)
+
         except Exception as err:
-            print("Can't open file: {} for date {}".format(file,download_file_day))
+            print(f"Can't open file: {file} for date {download_file_day}")
 
 
-def delete_files(ds, folder, file, **kwargs):
-    base_folder = 'data/staging/{}'.format(folder)
+def delete_files(ds, **kwargs):
+    base_folder = f"data/staging/"
     shutil.rmtree(base_folder, ignore_errors=True)
 
 
@@ -100,7 +98,7 @@ base_url = Variable.get("base_url")
 
 args = {
     'owner': 'airflow',
-    'description': 'Use of the DockerOperator',
+    'description': 'Download files for processing',
     'depend_on_past': False,
     'start_date': airflow.utils.dates.days_ago(2),
     'email_on_failure': False,
@@ -112,6 +110,7 @@ args = {
 dag = DAG(dag_id='download_files', default_args=args, schedule_interval=None, catchup=False)
 
 start = DummyOperator(task_id='start', dag=dag)
+wait = DummyOperator(task_id='wait', dag=dag)
 end = DummyOperator(task_id='end', dag=dag)
 
 download_tasks = []
@@ -120,7 +119,7 @@ delete_staging_files = []
 
 for t in config['etl_tasks']:
     download_tasks.append(PythonOperator(
-        task_id='download_{}'.format(t),
+        task_id=f"download_{t}",
         provide_context=True,
         op_kwargs=config['etl_tasks'][t],
         python_callable=download_files,
@@ -128,22 +127,21 @@ for t in config['etl_tasks']:
     ))
 
     decompress_tasks.append(PythonOperator(
-        task_id='decompress_{}'.format(t),
+        task_id=f"decompress_to_raw_{t}",
         provide_context=True,
         op_kwargs=config['etl_tasks'][t],
         python_callable=decompress_files,
         dag=dag,
     ))
 
-    # delete_staging_files.append(PythonOperator(
-    #     task_id='delete_staging_files_{}'.format(t),
-    #     provide_context=True,
-    #     op_kwargs=config['etl_tasks'][t],
-    #     python_callable=delete_files,
-    #     dag=dag,
-    # ))
-
-##delete_staging_files
+delete_staging_files = PythonOperator(
+    task_id="delete_staging_files",
+    provide_context=True,
+    python_callable=delete_files,
+    dag=dag,
+)
 
 for j in range(0, len(download_tasks)):
-    start >> download_tasks[j] >> decompress_tasks[j] >> end
+    start >> download_tasks[j] >> decompress_tasks[j] >> wait
+
+wait >> delete_staging_files >> end
